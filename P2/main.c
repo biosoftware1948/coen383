d038780ca8020efc6243d6124f3c8951652a9060
Kevin Velcich <kvelcich@gmail.com>
2019-01-29 20:05:50 -0800

    Cleaning main

--- P2/main.c
+++ P2/main.c
@@ -7,44 +7,41 @@
 #include "schedulingAlgorithms/HPFNP.h"
 #include "schedulingAlgorithms/HPFP.h"
 
-# define NUM_JOBS = 10;
+# define NUM_JOBS 10
 
 int main() {
-    //time chart
-    //Average turn around time
-    //Avereage waiting time
-    //Average response time
-    //Algorithm Throughput (HPF for priority level and all together)
-    // ensure calculation is only on jobs that ran
     Job* jobs = createJobs(NUM_JOBS);
     //sort_by_arrival_time(jobs, NUM_JOBS);
 
     // Printing out each job's name, arrival time, expected run time & priority
     report_print(jobs, NUM_JOBS);
 
-    //First Come First Serve
-    RunFCFS(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
+    // First Come First Serve
+    //RunFCFS(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
 
-    //Round Robin
-    // RunRR(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
-
-    //RunHPF(jobs, cpu)
-    //....
-    //...
-
-    Job *j1 = copyJobs(jobs, NUM_JOBS);//, *j2 = copyJobs(jobs), *j3 = copyJobs(jobs),
-    //    *j4 = copyJobs(jobs), *j5 = copyJobs(jobs), *j6 = copyJobs(jobs);
-    //Shortest Job First
+    // Shortest Job First
     //RunSJF(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
 
-    //Shortest Remaining Time
+    // Shortest Remaining Time
     //RunSRT(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
 
-    //Highest Priority First (Non-preemptive)
+    // Round Robin
+    //RunRR(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
+
+    //Highest Priority First (non-preemptive)
     //RunHPFNP(createCPU(), copyJobs(jobs, NUM_JOBS), NUM_JOBS);
 
-    //Highest Priority First (Preemptive)
+    //Highest Priority First (preemptive)
     //RunHPFP(createCPU(), j1, NUM_JOBS);
+
+
+    // TODO: Report Outputs
+    //time chart
+    //Average turn around time
+    //Avereage waiting time
+    //Average response time
+    //Algorithm Throughput (HPF for priority level and all together)
+    // ensure calculation is only on jobs that ran
 
     return 0;
 }
