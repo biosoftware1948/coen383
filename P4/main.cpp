308793e12116c01619ccd31d5b061ee1c09fff31
Esai Morales <esaimn96@gmail.com>
2019-02-17 10:19:39 -0800

    main readjusting page params

--- P4/main.cpp
+++ P4/main.cpp
@@ -6,13 +6,13 @@
 
 const unsigned QUANTUM = 100;
 extern const unsigned RUN_TIME = 60000; // milliseconds
-extern const unsigned JOB_COUNT = 20;
-extern const unsigned NUM_PAGES = 5;
+extern const unsigned JOB_COUNT = 150;
+extern const unsigned NUM_PAGES = 100;
 
 int main() {
     srand(time(NULL));
 
-    CPU cpu = CPU(FIFO);
+    CPU cpu = CPU(RANDOM);
     while (!cpu.isProcessesComplete()
             && cpu.getClockTime() <= RUN_TIME) {
         Process *p = cpu.getNextProcess();
